7eb2e144e8654ff80dbaa9465fa6e50380dd7649
MylesXP <mylesparkinson12@gmail.com>
2023-02-03 10:37:52 -0800

    Added an "opening" animation for add book button
    "Add book button" has an animation that will open up an reveal a form where the user can input the specifics of their desired book to store.

---
+++ app.js
@@ -0,0 +1,17 @@
+let addBookButton = document.getElementById('book_add__button');
+
+
+addBookButton.addEventListener('click', () => {
+    addBookButton.classList.toggle('width-extend')
+    console.log('hello')
+})
+
+let myLibrary = [];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
