0fc572bf1a7b16bc5c13e38d35a6de2573dfaa5d
MylesXP <mylesparkinson12@gmail.com>
2023-02-05 13:15:30 -0800

    Made open button untoggleable when opened
    open button to form toggle disabled to prevent user from accidentaly closing form.

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 let addBookButton = document.getElementById('book_add__button');
-
+let addBookText = document.getElementById('book_add__text');
 
 addBookButton.addEventListener('click', () => {
-    addBookButton.classList.toggle('width-extend')
+    addBookButton.classList.add('width-extend')
+    addBookText.classList.toggle('button-hide')
     console.log('hello')
 })
 
