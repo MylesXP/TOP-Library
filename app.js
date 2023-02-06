49d5d7a381eac907ce2c31b8f1fb90b101521eae
MylesXP <mylesparkinson12@gmail.com>
2023-02-05 18:27:40 -0800

    Added form to expanded button, configured js
    Created the form that comes after clicking on the "add book" button. Configured the JS to facilitate the transitions and the form's display.

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 let addBookButton = document.getElementById('book_add__button');
 let addBookText = document.getElementById('book_add__text');
+let addBookForm = document.getElementsByClassName('book-add-form')[0];
+let formCancelButton = document.getElementById('form_cancel_button');
+
 
 addBookButton.addEventListener('click', () => {
     addBookButton.classList.add('width-extend')
-    addBookText.classList.toggle('button-hide')
-    console.log('hello')
+    addBookText.classList.add('button-hide')
+    setTimeout(() => {
+        addBookForm.classList.add('book-add-form--display')
+    }, 700)
+    console.log('clicked')
+},false)
+
+formCancelButton.addEventListener('click', () => {
+    // addBookButton.classList.remove('book-add-form');
+    addBookButton.classList.remove('width-extend')
+    setTimeout(() => {
+        addBookText.classList.remove('button-hide')
+    }, 700);
+    addBookForm.classList.remove('book-add-form--display')
 })
+
+
 
 let myLibrary = [];
 
@@ -16,3 +33,12 @@ function Book() {
 function addBookToLibrary() {
   // do stuff here
 }
+
+function childClicked() {
+    console.log('child clicked')
+}
+
+function submitForm() {
+    addBookForm.submit();
+}
+
